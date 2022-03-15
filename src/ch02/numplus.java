5b6e9085cc3bc570fdbf63d7d1d2ade9f3c2dea1
ASTER18K <donhon05@gmail.com>
2022-03-15 12:11:38 +0900

    20220315 - 1

---
+++ src/ch02/numplus.java
@@ -0,0 +1,15 @@
+package ch02;
+
+// 정수를 입력 받아, 각 자리수의 합을 출력
+// 이벤트루프 문제 => while문으로 작성
+// 힌트 ==> n % 10은 1의 자리수 추출하는 것
+
+import java.util.Scanner;
+
+public class numplus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("정수 입력: ");
+        while ()
+    }
+}
